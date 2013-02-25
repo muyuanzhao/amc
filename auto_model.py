07c61ea34e20e0d8241c490f0aeca62a0ec2d9b9
MOON_CLJ <lijunli2598@gmail.com>
2013-02-25 14:39:47 +0800

    format clear

--- auto_model.py
+++ auto_model.py
@@ -4,6 +4,7 @@ from app import db
 
 __all__ = ['Credit', 'Customer', 'Delorder', 'Delorderinfo', 'Depart', 'Employee', 'Inaccount', 'Instockinform', 'Inventory', 'Lackorder', 'Msg', 'News', 'Orderinfo', 'Outaccount', 'Paybillaccount', 'Preorder', 'Preorderinfo', 'Product', 'Puraccount', 'Purorder', 'Reminder', 'Reminderinfo', 'Reorder', 'Sellaccount', 'Shoppingcart', 'Supplier', 'T_order']
 
+
 class Credit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customerId = db.Column(db.Integer)
@@ -12,6 +13,7 @@ class Credit(db.Model):
     accumulateDebts = db.Column(db.String(100))
     beizhu = db.Column(db.String(100))
 
+
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer)
@@ -19,6 +21,7 @@ class Customer(db.Model):
     address = db.Column(db.String(100))
     phone = db.Column(db.String(11))
     email = db.Column(db.String(100))
+
 
 class Delorder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,15 +32,18 @@ class Delorder(db.Model):
     driveTime = db.Column(db.String(19))
     delOrderStatus = db.Column(db.String(6))
 
+
 class Delorderinfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     delOrderId = db.Column(db.Integer)
     productId = db.Column(db.Integer)
     delNum = db.Column(db.Integer)
 
+
 class Depart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     departName = db.Column(db.String(10))
+
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,12 +54,14 @@ class Employee(db.Model):
     empAddress = db.Column(db.String(60))
     empPhone = db.Column(db.String(11))
 
+
 class Inaccount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     productId = db.Column(db.Integer)
     inNum = db.Column(db.Integer)
     inTime = db.Column(db.String(19))
     inPerson = db.Column(db.String(10))
+
 
 class Instockinform(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -63,10 +71,12 @@ class Instockinform(db.Model):
     time = db.Column(db.String(19))
     status = db.Column(db.String(6))
 
+
 class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     productId = db.Column(db.Integer)
     number = db.Column(db.Integer)
+
 
 class Lackorder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -76,6 +86,7 @@ class Lackorder(db.Model):
     lackTime = db.Column(db.String(19))
     lackOrderStatus = db.Column(db.String(6))
 
+
 class Msg(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(255))
@@ -83,11 +94,13 @@ class Msg(db.Model):
     content = db.Column(db.String(500))
     time = db.Column(db.String(19))
 
+
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     content = db.Column(db.String(100))
     time = db.Column(db.String(19))
+
 
 class Orderinfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -96,12 +109,14 @@ class Orderinfo(db.Model):
     orderNum = db.Column(db.Integer)
     orderInfoStatus = db.Column(db.String(10))
 
+
 class Outaccount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     productId = db.Column(db.Integer)
     outNum = db.Column(db.Integer)
     outTime = db.Column(db.String(19))
     outPerson = db.Column(db.String(10))
+
 
 class Paybillaccount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -114,6 +129,7 @@ class Paybillaccount(db.Model):
     accountTime = db.Column(db.String(19))
     payBillStatus = db.Column(db.String(6))
 
+
 class Preorder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     orderId = db.Column(db.Integer)
@@ -121,12 +137,14 @@ class Preorder(db.Model):
     preOrderTime = db.Column(db.String(19))
     preOrderStatus = db.Column(db.String(10))
 
+
 class Preorderinfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     preOrderId = db.Column(db.Integer)
     orderInfoId = db.Column(db.Integer)
     preOrderNum = db.Column(db.Integer)
     preOrderPerson = db.Column(db.String(10))
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -136,6 +154,7 @@ class Product(db.Model):
     buyPrice = db.Column(db.String(20))
     description = db.Column(db.String(500))
     type = db.Column(db.String(10))
+
 
 class Puraccount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -147,6 +166,7 @@ class Puraccount(db.Model):
     accountPerson = db.Column(db.String(10))
     accountTime = db.Column(db.String(19))
 
+
 class Purorder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     supplierId = db.Column(db.Integer)
@@ -156,6 +176,7 @@ class Purorder(db.Model):
     purTime = db.Column(db.String(19))
     getTime = db.Column(db.String(19))
     purOrderStatus = db.Column(db.String(6))
+
 
 class Reminder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -167,6 +188,7 @@ class Reminder(db.Model):
     recMoneyTime = db.Column(db.String(19))
     reminderStatus = db.Column(db.String(6))
 
+
 class Reminderinfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     reminderId = db.Column(db.Integer)
@@ -174,6 +196,7 @@ class Reminderinfo(db.Model):
     delNum = db.Column(db.Integer)
     price = db.Column(db.String(50))
     money = db.Column(db.String(100))
+
 
 class Reorder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -183,6 +206,7 @@ class Reorder(db.Model):
     reorderPerson = db.Column(db.String(10))
     reorderTime = db.Column(db.String(19))
     reorderStatus = db.Column(db.String(6))
+
 
 class Sellaccount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -194,6 +218,7 @@ class Sellaccount(db.Model):
     accountPerson = db.Column(db.String(10))
     accountTime = db.Column(db.String(19))
 
+
 class Shoppingcart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customerId = db.Column(db.Integer)
@@ -202,11 +227,13 @@ class Shoppingcart(db.Model):
     money = db.Column(db.String(500))
     flag = db.Column(db.Integer)
 
+
 class Supplier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     supplierName = db.Column(db.String(255))
     supplierAdd = db.Column(db.String(255))
     supplierPhone = db.Column(db.String(11))
+
 
 class T_order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
